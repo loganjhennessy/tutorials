880a0a6a0b4dd2200004933a29c4bbb61c454ab0
Logan Hennessy <loganjhennessy@gmail.com>
2019-04-27 17:53:15 -0700

    Finishing tutorial

--- qa-app/frontend/src/SecuredRoute/SecuredRoute.js
+++ qa-app/frontend/src/SecuredRoute/SecuredRoute.js
@@ -3,14 +3,15 @@ import {Route} from 'react-router-dom';
 import auth0Client from '../Auth';
 
 function SecuredRoute(props) {
-  const {component: Component, path} = props;
+  const {component: Component, path, checkingSession} = props;
   return (
     <Route path={path} render={() => {
-        if (!auth0Client.isAuthenticated()) {
-          auth0Client.signIn();
-          return <div></div>;
-        }
-        return <Component />
+      if (checkingSession) return <h3 className="text-center">Validating session</h3>;
+      if (!auth0Client.isAuthenticated()) {
+        auth0Client.signIn();
+        return <div></div>;
+      }
+      return <Component />
     }} />
   );
 }
